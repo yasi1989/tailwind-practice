39485def5d3550e5f58b5c292fde48a1ea6435bc
yasi1989 <kei.kobayashi.works1989@gmail.com>
2024-12-06 02:06:27 +0900

    first commit

---
+++ tailwind-portfolio/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        body: "#050505",
+        "selected-text": "#A3A3FF",
+        "theme": "#5c318c",
+      },
+    },
+    fontFamily:{
+      poppins: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
